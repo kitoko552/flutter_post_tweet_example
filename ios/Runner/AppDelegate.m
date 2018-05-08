aba9cad7c3d155c792efcd4b42ad63c6702b543e
Kosuke Kito <kito_kosuke@cyberagent.co.jp>
2018-05-08 13:53:17 +0900

    Refactor

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,28 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
+#import <Social/Social.h>
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    FlutterMethodChannel* channel = [FlutterMethodChannel
+                                            methodChannelWithName:@"com.kitoko552.flutter_post_tweet_example/tweet"
+                                            binaryMessenger:controller];
+    
+    __weak AppDelegate* wself = self;
+    [channel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        [wself tweet];
+    }];
+    
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
+
+- (void)tweet {
+    // tweet by using TwitterKit or other service
 }
 
 @end
